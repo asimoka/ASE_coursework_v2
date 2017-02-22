af5e3d457897649af08c866641abc7df0142c3d6
Kirsty <kirstyroxanaduncan@gmail.com>
2017-02-22 13:30:33 +0000

    Update RideListManager.java

--- src/rides/RideListManager.java
+++ src/rides/RideListManager.java
@@ -1,21 +1,104 @@
 package rides;
+/*
+ * This class deals with all of the I/O between the program
+ * and the input/output text files. 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class RideListManager {
 
-	private RideList RideList;
-	private String taxiRegNumber;
-	private String destination;
-	private Double travelDistance;
-	private int numPassengers;
-	private Double fare;
-	private String driverName;
-	private String carType;
+	private RideList rideList;
+	private String	 taxiRegNumber;
+	private String 	 destination;
+	private Double 	 travelDistance;
+	private int 	 numPassengers;
+	private Double 	 fare;
+	private String 	 driverName;
+	private String 	 carType;
 
-	public void readerJourney() {
+	public RideListManager()
+	{
+		//Initialize empty list of Competitors
+    	rideList = new RideList();
+        addRides(); 
+        
+	}
+	
+	 private void addRides() 
+	    {
+	        //load staff data from file
+	        BufferedReader RidesBuff = null;
+	        BufferedReader destBuff = null;
+	        BufferedReader taxiBuff = null;
+	        
+	        try 
+	        {
+	        
+	        	RidesBuff = new BufferedReader(new FileReader("journeyDetailsInput.csv"));
+		    	String inputJourneyLine = RidesBuff.readLine();  //read first line
+		    	while(inputJourneyLine != null)
+		    	{  
+		    		//stores details from this line in RideList class
+		    		processLineJourney(inputJourneyLine);
+		            //read next line
+		            inputJourneyLine = RidesBuff.readLine();
+		        }
+		    	
+		    	destBuff = new BufferedReader(new FileReader("destinationDetailsInput.csv"));
+		    	String inputDestLine = destBuff.readLine();  //read first line
+		    	while(inputDestLine != null)
+		    	{  
+		    		//stores details from this line in RideList class
+		    		processLineDestination(inputDestLine);
+		            //read next line
+		            inputDestLine = destBuff.readLine();
+		        }
+		    	
+		    	taxiBuff = new BufferedReader(new FileReader("taxiDetailsInput.csv"));
+		    	String inputTaxiLine = taxiBuff.readLine();  //read first line
+		    	while(inputTaxiLine != null)
+		    	{  
+		    		//stores details from this line in RideList class
+		    		processLineTaxi(inputTaxiLine);
+		            //read next line
+		            inputTaxiLine = taxiBuff.readLine();
+		        }
+	        }
+	        catch(FileNotFoundException e) 
+	        {
+	        	System.out.println(e.getMessage());
+	            System.exit(1);
+	        }
+	        catch (IOException e) 
+	        {
+	        	e.printStackTrace();
+	            System.exit(1);        	
+	        }
+	        finally  
+	        {
+	        	try
+	        	{
+	        		RidesBuff.close();
+	        	}
+	        	catch (IOException ioe) 
+	        	{
+	        		//don't do anything
+	        	}
+	        }   	
+	    }
+	 //
+	public void readerJourney(String filename) {
 		try{
-			File inFile = new File("journeyDetailsInput.csv");
+			File inFile = new File(filename);
 
 			Scanner input = new Scanner(inFile);
 			String journeys = input.nextLine();
@@ -28,9 +111,9 @@ public class RideListManager {
 			System.out.printf("ERROR: File Not Found", ex);
 		}
 	}
-	public void readerDestination() {
+	public void readerDestination(String filename) {
 		try{
-			File inFile = new File("destinationDetailsInput.csv");
+			File inFile = new File(filename);
 
 			Scanner input = new Scanner(inFile);
 			String destination = input.nextLine();
@@ -68,7 +151,7 @@ public class RideListManager {
 			fare = Double.parseDouble(inline[4]);
 
 			Journey j = new Journey(taxiRegNumber, destination, travelDistance, numPassengers, fare);
-			RideList.addJourney(j);
+			rideList.addJourney(j);
 		} catch (NumberFormatException nfe) {
 			String error = "Number conversion error in '"
 					+ line + "' - " + nfe.getMessage();
@@ -92,7 +175,7 @@ public class RideListManager {
 			carType = inline[2];
 
 			Taxi j = new Taxi(taxiRegNumber, driverName, carType);
-			RideList.addTaxi(j);
+			rideList.addTaxi(j);
 		} catch (NumberFormatException nfe) {
 			String error = "Number conversion error in '"
 					+ line + "' - " + nfe.getMessage();
@@ -114,7 +197,7 @@ public class RideListManager {
 			travelDistance = Double.parseDouble(inline[1]);
 
 			Destination j = new Destination(destination, travelDistance);
-			RideList.addDestination(j);
+			rideList.addDestination(j);
 		} catch (NumberFormatException nfe) {
 			String error = "Number conversion error in '"
 					+ line + "' - " + nfe.getMessage();
@@ -128,6 +211,44 @@ public class RideListManager {
 			System.out.println(error);
 		}
 	}
+	
+	public String outputJourneysByYear()
+	{
+		String output="";
+		
+		ArrayList<String> journeysThisYear  = rideList.getDestinationsThisYear();
+		ArrayList<String> journeysLastYear  = rideList.getDestinationsLastYear();
+		ArrayList<String> journeysBothYears = rideList.getDestinationsBothYears();
+		
+		output += journeysThisYear.size() + " NEW PLACES IN 2017.";
+		
+		for (String j : journeysThisYear) {
+			output += j + "\n";
+		}
+
+		output += journeysLastYear.size() + " PLACES VISITED IN 2016 ONLY.";
+		
+		for (String j : journeysLastYear) {
+			output += j + "\n";
+		}
+
+		output += journeysBothYears.size() + " PLACES VISITED IN BOTH 2016 AND 2017.";
+		
+		for (String j : journeysBothYears) {
+			output += j + "\n";
+		}
+
+		
+		return output;
+	}
+	
+	public static void main (String arg[]) 
+    {
+       	//creates demo object which sets up the interface
+    	//then just waits for user interaction
+    	RideListManager rideListMan = new RideListManager();   	
+    	System.out.print(rideListMan.rideList.outputTreeByDrivers());
+    	//System.out.print(rideListMan.RideList.getDestination(1));
+    }        
 
 }
-

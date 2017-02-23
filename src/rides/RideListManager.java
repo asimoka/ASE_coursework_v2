6e8426bb33a2c9940072080ad7a5c8e60c26eb7f
Kirsty <kirstyroxanaduncan@gmail.com>
2017-02-23 17:38:35 +0000

    Update RideListManager.java

--- src/rides/RideListManager.java
+++ src/rides/RideListManager.java
@@ -2,13 +2,15 @@ package rides;
 /*
  * This class deals with all of the I/O between the program
  * and the input/output text files. 
- * reads input files, processes each line of the files then splits them into instance variables for each class
- * performs various checks to make sure data entered follows common format for names, places, registration numbers, distance, car types, and number of passengers
  * 
- * code authors Kirsty Duncan and Assem Yerkinova
+ * 
+ * 
+ * 
  * 
  */
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Set;
 
 public class RideListManager {
@@ -21,93 +23,106 @@ public class RideListManager {
 	private String 	 driverName;
 	private String 	 carType;
 
-	//constructor for RideListManager
-	public RideListManager() {
+	public RideListManager(String taxiInput, String destinationInput, String journeysInput, String lastYearInput)
+	{
 		//Initialize empty list of Competitors
 		rideList = new RideList();
-		addRides();
+		addRides(taxiInput, destinationInput, journeysInput, lastYearInput); 
+
+	}
+	
+	
+	private void addRides(String taxiInput, String destinationInput, String journeysInput, String lastYearInput) 
+	{
+		//load data from files;
+		BufferedReader taxiBuff = null;
+		BufferedReader destBuff = null;
+		BufferedReader journeyBuff = null;
+		BufferedReader lastYearBuff = null;
+
+		try 
+		{
+			//create buffer for taxi file and read each line
+			taxiBuff = new BufferedReader(new FileReader(taxiInput));
+			String inputTaxiLine = taxiBuff.readLine();  //read first line
+			while(inputTaxiLine != null)
+			{  
+				//stores details from this line in RideList class
+				processLineTaxi(inputTaxiLine);
+				//read next line
+				inputTaxiLine = taxiBuff.readLine();
+			}
+			//create buffer for destination file and read each line
+			destBuff = new BufferedReader(new FileReader(destinationInput));
+			String inputDestLine = destBuff.readLine();  //read first line
+			while(inputDestLine != null)
+			{  
+				//stores details from this line in RideList class
+				processLineDestination(inputDestLine);
+				//read next line
+				inputDestLine = destBuff.readLine();
+			}
+			//create buffer for journeys file and read each line
+			journeyBuff = new BufferedReader(new FileReader(journeysInput));
+			String inputJourneyLine = journeyBuff.readLine();  //read first line
+			while(inputJourneyLine != null)
+			{  
+				//stores details from this line in RideList class
+				processLineJourney(inputJourneyLine);
+				//read next line
+				inputJourneyLine = journeyBuff.readLine();
+			}
+			//create buffer for last year's destinations file and read each line
+			lastYearBuff = new BufferedReader(new FileReader(lastYearInput));
+			String inputLastYearLine = lastYearBuff.readLine();  //read first line
+			while(inputLastYearLine != null)
+			{  
+				//stores details from this line in RideList class
+				processLineLastYear(inputLastYearLine);
+				//read next line
+				inputLastYearLine = lastYearBuff.readLine();
+			}
 		}
-		//load data from files destinationDetailsInput.csv journeyDetailsInput.csv and taxiDetailsInput.csv
-		 private void addRides() {
-			//construct null buffered readers for input
-			BufferedReader RidesBuff = null;
-			BufferedReader destBuff = null;
-			BufferedReader taxiBuff = null;
-	        
-			try {
-				//create buffer for journeys file and read each line
-				RidesBuff = new BufferedReader(new FileReader(".//src/journeyDetailsInput.csv"));
-				String inputJourneyLine = RidesBuff.readLine();  //read first line
-				while(inputJourneyLine != null) {  
-					//stores details from this line in RideList class
-					processLineJourney(inputJourneyLine);
-				    //read next line
-				    inputJourneyLine = RidesBuff.readLine();
-				}
-				//create buffer for destination file and read each line
-				destBuff = new BufferedReader(new FileReader(".//src/destinationDetailsInput.csv"));
-				String inputDestLine = destBuff.readLine();  //read first line
-				while(inputDestLine != null) {  
-					//stores details from this line in RideList class
-					processLineDestination(inputDestLine);
-					//read next line
-					inputDestLine = destBuff.readLine();
-				}
-				//create buffer for taxi file and read each line
-				taxiBuff = new BufferedReader(new FileReader(".//src/taxiDetailsInput.csv"));
-				String inputTaxiLine = taxiBuff.readLine();  //read first line
-				while(inputTaxiLine != null) {  
-					//stores details from this line in RideList class
-					processLineTaxi(inputTaxiLine);
-					//read next line
-					inputTaxiLine = taxiBuff.readLine();
-				}
-				taxiBuff = new BufferedReader(new FileReader(".//src/destination2016.csv"));
-				String inputLastYearLine = taxiBuff.readLine();  //read first line
-				while(inputLastYearLine != null) {  
-					//stores details from this line in RideList class
-					processLineLastYear(inputLastYearLine);
-					//read next line
-					inputLastYearLine = taxiBuff.readLine();
-				}
-			}
-			//catches if no file was found
-			catch(FileNotFoundException e) 
-			{
-				System.out.println(e.getMessage());
-				System.exit(1);
-			}
-			//catches if there is a problem with data in the file
-			catch (IOException e) 
-			{
-				e.printStackTrace();
-				System.exit(1);        	
-			}
-			finally  
-			{
+		//catches if no file was found
+		catch(FileNotFoundException e) 
+		{
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+			System.exit(1);        	
+		}
+		finally  
+		{
 			try
-			//catches if theres a problem closing RidesBuff
 			{
-				RidesBuff.close();
+				taxiBuff.close();
+				destBuff.close();
+				journeyBuff.close();
+				lastYearBuff.close();
 			}
 			catch (IOException ioe) 
 			{
-			//don't do anything
+				//don't do anything
 			}
 		}   	
 	}
-	//method to process lines from input and convert from one String to various instance variables in the Journey class
+	//
+
 	public void processLineJourney(String line) {
 		try {
 			//split up the information in this line by commas into license plate number, destination, distance and number of passengers
 			String [] inline = line.split(",");
 			taxiRegNumber = inline[0];
 			destination = inline[1];
-			//convert these values to the correct format
-			travelDistance = Double.parseDouble(inline[3]);
+			//convert numerical value to the correct format
+			//travelDistance = Double.parseDouble(inline[3]);
 			numPassengers = Integer.parseInt(inline[2]);
+
 			//create a new object of type journey and add it to the list of journeys
-			Journey j = new Journey(taxiRegNumber, destination, travelDistance, numPassengers);
+			Journey j = new Journey(taxiRegNumber, destination, numPassengers);
 			rideList.addJourney(j);
 		}
 		//if one of the numeric values is not in the correct format to be converted to double/integer
@@ -123,18 +138,26 @@ public class RideListManager {
 					+ "' index position : " + air.getMessage();
 			System.out.println(error);
 		}
+		catch (noMatchingDestinationException nmde) {
+			//just don't add this line
+		}
+		catch (noMatchingDriverException nmde) {
+			//just don't add this line
+		}
 
 	}
-	//method to process lines from input and convert from one String to various instance variables in the Taxi class
+
 	public void processLineTaxi(String line) {
 		try {
+			//split input line by commas
 			String [] inline = line.split(",");
+			//retrieve taxi details
 			taxiRegNumber = inline[1];
 			driverName = inline[0];
 			carType = inline[2];
-
-			Taxi j = new Taxi(taxiRegNumber, driverName, carType);
-			rideList.addTaxi(j);
+			//create new instance of taxi with the above details and add it to the list of valid taxis
+			Taxi t = new Taxi(taxiRegNumber, driverName, carType);
+			rideList.addTaxi(t);
 		}
 		//if license plate was not in the correct format
 		catch (incorrectLicensePlateException ilpe) {
@@ -149,9 +172,9 @@ public class RideListManager {
 					+ "' index position : " + air.getMessage();
 			System.out.println(error);
 		}
-	
+
 	}
-	//method to process lines from input and convert from one String to various instance variables in the Destination class
+
 	public void processLineDestination(String line) {
 		try {
 			//split the line up into destination name and travel distance
@@ -163,6 +186,7 @@ public class RideListManager {
 			Destination j = new Destination(destination, travelDistance);
 			//add to the arraylist for valid destinations
 			rideList.addDestination(j);
+		}
 		//this catches missing items if only one or two items
 		//other omissions will result in other errors
 		catch (ArrayIndexOutOfBoundsException air) {
@@ -171,7 +195,7 @@ public class RideListManager {
 			System.out.println(error);
 		}
 	}
-	//method to process lines from destinations2016.csv
+
 	public void processLineLastYear(String line) {
 		try {
 			//add this journey destination to the arraylist
@@ -184,9 +208,11 @@ public class RideListManager {
 					+ "' index position : " + air.getMessage();
 			System.out.println(error);
 		}
-		
+
 	}
-	//method to output to text file journeys organize by year
+
+
+
 	public String outputJourneysByYear()
 	{
 		String output="";
@@ -194,7 +220,7 @@ public class RideListManager {
 		Set<String> journeysThisYear  = rideList.getDestinationsThisYear();
 		Set<String> journeysLastYear  = rideList.getDestinationsLastYear();
 		Set<String> journeysBothYears = rideList.getDestinationsBothYears();
-		
+
 		output += journeysThisYear.size() + " NEW PLACES IN 2017. \n";
 		//print each journey visited this year only on a separate line
 		for (String j : journeysThisYear) {
@@ -215,45 +241,73 @@ public class RideListManager {
 		//return the full report as a string to be output
 		return output;
 	}
-	
+
 	//method that creates a string version of the treemap values ready for output
 	public String outputTreeByDrivers()
 	{
-		return rideList.outputTreeByDrivers();
+		//return rideList.outputTreeByDrivers();
+		String output="";
+		Set< Map.Entry< String,ArrayList<String>> > treeSetView = rideList.treeByDrivers().entrySet();
+		for (Map.Entry< String,ArrayList<String>> byDrivers:treeSetView)
+		{
+			output+=byDrivers.getKey()+":\n";
+			for (String destinations:byDrivers.getValue())
+			{
+				output+="     "+destinations+"\n";
+			}
+		}
+		return output;
 	}
-	
+
 	//returns a report of the 5 most expensive journeys
 	public String getFiveExpensiveCheapestJourney() {
-		return rideList.getFiveExpensiveCheapestJourney();
-	}
-	
-
-	
-	//method to write the report into output file
-		public  void writeToFile(String filename, String report) 
-		{	
-			FileWriter fw;
-			 try 
-			 {
-			    fw = new FileWriter(filename);
-			    fw.write(report+"\n\n");
-			  	fw.close();
-			 }
-			 //message and stop if file not found
-			 catch (FileNotFoundException fnf)
-			 {
-				 System.out.println(filename + " not found ");
-				 System.exit(0);
-			 }
-			 //stack trace here because we don't expect to come here
-			 catch (IOException ioe)
-			 {
-			    ioe.printStackTrace();
-			    System.exit(1);
-			 }
-			 
-			 
+		//add a formatted list of the top 5 most expensive journeys to the output string
+		String fiveExpJourney="CHARGES FOR THE TOP 5 LEAST EXPENSIVE JOURNEYS \n";
+		//get an ordered list of journeys by fare price
+		ArrayList<Journey> sortedJourneys = rideList.sortByFarePrice();
+		//add the first five of the ordered list to the output string on a new line
+		for (int i=0;i<5;i++)
+		{
+			fiveExpJourney += sortedJourneys.get(i).toString()+"\n";
 		}
-		
+		//add a formatted list of the top 5 least expensive journeys to the output string
+		fiveExpJourney +="\n\nCHARGES FOR THE TOP 5 MOST EXPENSIVE JOURNEYS \n";
+		int l = sortedJourneys.size();
+		//add the last five of the ordered list to the output string on a new line
+		for (int i=l-5;i<l;i++)
+		{
+			fiveExpJourney += sortedJourneys.get(i).toString()+"\n";
+		}
+		return fiveExpJourney;
+	}
+
+
+
+	//method to write the report into output file
+	public  void writeToFile(String filename, String report) 
+	{	
+		FileWriter fw;
+		try 
+		{
+			fw = new FileWriter(filename);
+			fw.write(report+"\n\n");
+			fw.close();
+		}
+		//message and stop if file not found
+		catch (FileNotFoundException fnf)
+		{
+			System.out.println(filename + " not found ");
+			System.exit(0);
+		}
+		//stack trace here because we don't expect to come here
+		catch (IOException ioe)
+		{
+			ioe.printStackTrace();
+			System.exit(1);
+		}
+
+
+	}
+
 
 }

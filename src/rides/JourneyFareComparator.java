5bf77a4f399f1286f742c855edcfd549396c10c1
asimoka <assemyerkinova@gmail.com>
2017-02-23 15:17:24 +0000

    comments

--- src/rides/JourneyFareComparator.java
+++ src/rides/JourneyFareComparator.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 
 public class JourneyFareComparator implements Comparator<Journey>{
 
-	//defines an ordering on competitors objects on the name
+	//defines an ordering of the list according to the fare of the journey
 	public int compare(Journey j1, Journey j2) 
 	{
 		return j1.getFare().compareTo(j2.getFare());

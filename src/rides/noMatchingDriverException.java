1faaf60f72b2a65bcb7354c855351a297420a790
asimoka <assemyerkinova@gmail.com>
2017-02-23 15:19:22 +0000

    comments

--- src/rides/noMatchingDriverException.java
+++ src/rides/noMatchingDriverException.java
@@ -1,8 +1,12 @@
 package rides;
 
+  /*exception class that throws an error 
+   *if the given plate number of wrong format (not corresponding
+   to the given format)*/
 public class noMatchingDriverException extends Exception {
 	
 	public  noMatchingDriverException (String noMatch){
+		//message to be displayed in case of mismatch
 		super("There is no taxi with this plate Number = " + noMatch);
 	}
 }

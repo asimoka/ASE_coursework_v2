315bef0fd5e59ddc9cef0cb9aea65f57682565d5
Kirsty <kirstyroxanaduncan@gmail.com>
2017-03-08 10:27:28 +0000

    Update Journey.java

--- src/rides/Journey.java
+++ src/rides/Journey.java
@@ -1,55 +1,71 @@
 package rides;
+/* Represents a single journey in a taxi
+ * A journey has a taxi license plate number,
+ * destination, distance, number of passengers
+ * and a fare.
+ * Includes Methods to get and set variables as well as
+ * calculate the fare for the journey,
+ * check that license plate number is valid
+ * and represent a journey as a string
+ * in a meaningful format.
+ */
 
 public class Journey {
+	//instance variables
 	private String taxiRegNumber;
 	private String destination;
 	private Double travelDistance;
 	private int numPassengers;
 	private Double fare;
 
-	public Journey(String tRegNo, String dest, Double tDist, int numPass, Double fare) {
-		if (validPlateNumber(tRegNo)) {
-			this.taxiRegNumber=tRegNo;
-		}
-		else {
-			throw new InputMismatchException("Licence Plate number is in the wrong format!");
-		}
+	//constructor
+	public Journey(String tRegNo, String dest, int numPass) { 
+		
+		this.taxiRegNumber=tRegNo;
 		this.destination=dest;
-		this.travelDistance=tDist;
+		this.travelDistance=0.0;
 		this.numPassengers=numPass;
-		this.fare=fare;
+		this.fare=calculateFare();
 	}
+	
+	//calculate the fare for this journey
+	public Double calculateFare() {
+		double taxiFare = 0;
+		double halfPriceDestination=0;
+		double fullPriceDestination=0;
+		//if a journey has distance greater than 10 miles, any mile after 10 is half price
+		if (travelDistance>10) {
+			halfPriceDestination=travelDistance-10;
+			fullPriceDestination=10.0;
+			taxiFare=(fullPriceDestination*2+halfPriceDestination);
+			}
+	 	else {
+	 		//if a journey is less than 10 miles, it is full price. Fare = 2* distance in miles
+	 		taxiFare=(travelDistance*2);
+	 		}
+		//if a journey is less than 1.5 miles, charge £3
+		if (taxiFare<3){
+			taxiFare=3.0;
+			}
+		return taxiFare;
+	}
+	
 	
 	//getter and setter for taxiRegNumber
 	public String getTaxiRegNumber() {
 		return taxiRegNumber;
 	}
 	
-	public void setTaxiRegNumber(String taxiRegNumber) {
+	//set taxi license plate number. If plate number is in the wrong format, throw an exception
+	public void setTaxiRegNumber(String taxiRegNumber) throws incorrectLicensePlateException {
 		if (validPlateNumber(taxiRegNumber)) {
 			this.taxiRegNumber = taxiRegNumber;
 		}
 		else {
-			throw new InputMismatchException("Licence Plate number is in the wrong format!");
+			throw new incorrectLicensePlateException(taxiRegNumber);
 		}
 	}
-	//check that license plate number is in the correct format
-	public boolean validPlateNumber(String plateNumber) {
-		//check license plate has the correct length
-		if (plateNumber.length() != 7) {
-			return false;
-		}
-		//check that licence plate has format letter, letter, digit, digit, letter, letter, letter
-		if (Character.isLetter(plateNumber.charAt(0)) && Character.isLetter(plateNumber.charAt(2))
-				&& Character.isDigit(plateNumber.charAt(3)) && Character.isDigit(plateNumber.charAt(4))
-				&& Character.isLetter(plateNumber.charAt(5)) && Character.isLetter(plateNumber.charAt(6))
-				&& Character.isLetter(plateNumber.charAt(7))) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+
 	
 	//getter and setter for destination
 	public String getDestination() {
@@ -83,14 +99,33 @@ public class Journey {
 		this.fare = fare;
 	}
 	
+	//returns a formatted string describing the journey
 	public String toString() {
-		String outputLine="\n"+String.format("%-10s", this.taxiRegNumber); 
-		outputLine+=String.format("%-25s", this.destination) ;
-		outputLine+=String.format("%4s", this.travelDistance)  ;
-		outputLine+= " miles, "+String.format("%-2d", this.numPassengers)+ " people, " ;
-		outputLine+=" Cost:" + String.format("%4s", this.fare);
-		 return outputLine;
+		return "" + String.format("%-10s", this.taxiRegNumber) + 
+			String.format("%-28s", this.destination) +
+			String.format("%4s", this.travelDistance)  +
+			" miles, "+String.format("%-2d", this.numPassengers)+ " people, " +
+			" Cost:" + String.format("%4s", this.fare);
 	}
+	
+	//check that license plate number is in the correct format
+	public boolean validPlateNumber(String plateNumber) {
+		//check license plate has the correct length
+		if (plateNumber.trim().length() != 7) {
+			return false;
+		}
+		//check that licence plate has format letter, letter, digit, digit, letter, letter, letter
+		if (Character.isLetter(plateNumber.charAt(0)) && Character.isLetter(plateNumber.charAt(1))
+				&& Character.isDigit(plateNumber.charAt(2)) && Character.isDigit(plateNumber.charAt(3))
+				&& Character.isLetter(plateNumber.charAt(4)) && Character.isLetter(plateNumber.charAt(5))
+				&& Character.isLetter(plateNumber.charAt(6))) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
 }
 
 

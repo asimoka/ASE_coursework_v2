7e5f88ca5eb75d95dff2fe23317fd726ea9cd360
Kirsty <kirstyroxanaduncan@gmail.com>
2017-03-08 10:26:39 +0000

    Update Destination.java

--- src/rides/Destination.java
+++ src/rides/Destination.java
@@ -1,14 +1,19 @@
 package rides;
 
+/* Represents a valid destination destination.
+ * a destination has destination name and distance in miles*/
+
 public class Destination {
+	//instance variables
 	private String destinationName;
 	private double distance;
 	
+	//constructor
 	public Destination (String destName, double distance){
 		this.destinationName=destName;
 		this.distance=distance;
 	}
-
+	//getters and setters
 	public String getDestinationName() {
 		return destinationName;
 	}
